0725f4ddf3328abb5b5272224ce1b39f196aa8ef
Ichchha Moktan <ichchhamoktan07@gmail.com>
2022-03-23 16:50:55 +0545

    reduce thread delay from 10 to 5 seconds for starting foreground notification

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
@@ -830,11 +830,9 @@ public class MapboxNavigation implements ServiceConnection {
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
-        Log.d("NavigationService", "Connected to service.");
         NavigationService.LocalBinder binder = (NavigationService.LocalBinder) service;
         navigationService = binder.getService();
         navigationService.startNavigation(this);
-        Log.d("NavigationService", "Connected to service.1");
         isBound = true;
     }
 
--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationService.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationService.java
@@ -33,155 +33,123 @@ import timber.log.Timber;
  */
 public class NavigationService extends Service {
 
-  private final IBinder localBinder = new LocalBinder();
-  private RouteProcessorBackgroundThread thread;
-  private NavigationLocationEngineUpdater locationEngineUpdater;
-  private RouteFetcher routeFetcher;
-  private NavigationNotificationProvider notificationProvider;
+    private final IBinder localBinder = new LocalBinder();
+    private RouteProcessorBackgroundThread thread;
+    private NavigationLocationEngineUpdater locationEngineUpdater;
+    private RouteFetcher routeFetcher;
+    private NavigationNotificationProvider notificationProvider;
 
-  @Nullable
-  @Override
-  public IBinder onBind(Intent intent) {
-    return localBinder;
-  }
-
-  /**
-   * Only should be called once since we want the service to continue running until the navigation
-   * session ends.
-   */
-  @Override
-  public int onStartCommand(Intent intent, int flags, int startId) {
-    Log.d("NavigationService", "Connected to service.7");
-    NavigationTelemetry.getInstance().initializeLifecycleMonitor(getApplication());
-    Log.d("NavigationService", "Connected to service.8");
-    return START_STICKY;
-  }
-
-  @Override
-  public void onDestroy() {
-    stopForeground(true);
-    super.onDestroy();
-  }
-
-  /**
-   * This gets called when {@link MapboxNavigation#startNavigation(DirectionsRoute)} is called and
-   * setups variables among other things on the Navigation Service side.
-   */
-  void startNavigation(MapboxNavigation mapboxNavigation) {
-    Log.d("NavigationService", "Connected to service.3");
-    initialize(mapboxNavigation);
-    Log.d("NavigationService", "Connected to service.4");
-    new Handler(Looper.getMainLooper()).post(new Runnable() {
-      public void run() {
-        startForegroundNotification(notificationProvider.retrieveNotification());
-        try {
-          Thread.sleep(10000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
-    });
-    Log.d("NavigationService", "Connected to service.5");
-    locationEngineUpdater.forceLocationUpdate(mapboxNavigation.getRoute());
-    Log.d("NavigationService", "Connected to service.6");
-  }
-
-  /**
-   * Removes the location / route listeners and  quits the thread.
-   */
-  void endNavigation() {
-    routeFetcher.clearListeners();
-    locationEngineUpdater.removeLocationEngineListener();
-    notificationProvider.shutdown(getApplication());
-    thread.quit();
-  }
-
-  /**
-   * Called with {@link MapboxNavigation#setLocationEngine(LocationEngine)}.
-   * Updates this service with the new {@link LocationEngine}.
-   *
-   * @param locationEngine to update the provider
-   */
-  void updateLocationEngine(LocationEngine locationEngine) {
-    locationEngineUpdater.updateLocationEngine(locationEngine);
-  }
-
-  private void initialize(final MapboxNavigation mapboxNavigation) {
-    NavigationEventDispatcher dispatcher = mapboxNavigation.getEventDispatcher();
-    String accessToken = mapboxNavigation.obtainAccessToken();
-    Log.d("NavigationService", "Connected to service.9");
-    initializeRouteFetcher(dispatcher, accessToken, mapboxNavigation.retrieveEngineProvider());
-    Log.d("NavigationService", "Connected to service.10");
-//    new Handler(Looper.getMainLooper()).post(new Runnable() {
-//      public void run() {
-        initializeNotificationProvider(mapboxNavigation);
-//        try {
-//          Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//          Log.d("NavigationService", "initializeNotificationProvider exception");
-//          e.printStackTrace();
-//        }
-//      }
-//    });
-    Log.d("NavigationService", "Connected to service.11");
-    initializeRouteProcessorThread(dispatcher, routeFetcher, notificationProvider);
-    Log.d("NavigationService", "Connected to service.12");
-    initializeLocationProvider(mapboxNavigation);
-  }
-
-  private void initializeRouteFetcher(NavigationEventDispatcher dispatcher, String accessToken,
-                                      NavigationEngineFactory engineProvider) {
-    FasterRoute fasterRouteEngine = engineProvider.retrieveFasterRouteEngine();
-    NavigationFasterRouteListener listener = new NavigationFasterRouteListener(dispatcher, fasterRouteEngine);
-    routeFetcher = new RouteFetcher(getApplication(), accessToken);
-    routeFetcher.addRouteListener(listener);
-  }
-
-  private void initializeNotificationProvider(MapboxNavigation mapboxNavigation) {
-    Log.d("NavigationService", "initializeNotificationProvider: 1");
-    notificationProvider = new NavigationNotificationProvider(getApplication(), mapboxNavigation);
-    Log.d("NavigationService", "initializeNotificationProvider: 2");
-  }
-
-  private void initializeRouteProcessorThread(NavigationEventDispatcher dispatcher, RouteFetcher routeFetcher,
-                                              NavigationNotificationProvider notificationProvider) {
-    RouteProcessorThreadListener listener = new RouteProcessorThreadListener(
-      dispatcher, routeFetcher, notificationProvider
-    );
-    thread = new RouteProcessorBackgroundThread(new Handler(), listener);
-  }
-
-  private void initializeLocationProvider(MapboxNavigation mapboxNavigation) {
-    LocationEngine locationEngine = mapboxNavigation.getLocationEngine();
-    int accuracyThreshold = mapboxNavigation.options().locationAcceptableAccuracyInMetersThreshold();
-    LocationValidator validator = new LocationValidator(accuracyThreshold);
-    NavigationLocationEngineListener listener = new NavigationLocationEngineListener(
-      thread, mapboxNavigation, locationEngine, validator
-    );
-    locationEngineUpdater = new NavigationLocationEngineUpdater(locationEngine, listener);
-  }
-
-  private void startForegroundNotification(NavigationNotification navigationNotification) {
-    final Notification notification = navigationNotification.getNotification();
-    final int notificationId = navigationNotification.getNotificationId();
-    notification.flags = Notification.FLAG_FOREGROUND_SERVICE;
-//    new Handler(Looper.getMainLooper()).post(new Runnable() {
-//      public void run() {
-        startForeground(notificationId, notification);
-//        try {
-//          Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//          e.printStackTrace();
-//        }
-//      }
-//    });
-//    startForeground(notificationId, notification);
-  }
-
-  class LocalBinder extends Binder {
-    NavigationService getService() {
-      Timber.d("Local binder called.");
-      return NavigationService.this;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return localBinder;
     }
-  }
+
+    /**
+     * Only should be called once since we want the service to continue running until the navigation
+     * session ends.
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        NavigationTelemetry.getInstance().initializeLifecycleMonitor(getApplication());
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopForeground(true);
+        super.onDestroy();
+    }
+
+    /**
+     * This gets called when {@link MapboxNavigation#startNavigation(DirectionsRoute)} is called and
+     * setups variables among other things on the Navigation Service side.
+     */
+    void startNavigation(MapboxNavigation mapboxNavigation) {
+        initialize(mapboxNavigation);
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            public void run() {
+                startForegroundNotification(notificationProvider.retrieveNotification());
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        locationEngineUpdater.forceLocationUpdate(mapboxNavigation.getRoute());
+    }
+
+    /**
+     * Removes the location / route listeners and  quits the thread.
+     */
+    void endNavigation() {
+        routeFetcher.clearListeners();
+        locationEngineUpdater.removeLocationEngineListener();
+        notificationProvider.shutdown(getApplication());
+        thread.quit();
+    }
+
+    /**
+     * Called with {@link MapboxNavigation#setLocationEngine(LocationEngine)}.
+     * Updates this service with the new {@link LocationEngine}.
+     *
+     * @param locationEngine to update the provider
+     */
+    void updateLocationEngine(LocationEngine locationEngine) {
+        locationEngineUpdater.updateLocationEngine(locationEngine);
+    }
+
+    private void initialize(final MapboxNavigation mapboxNavigation) {
+        NavigationEventDispatcher dispatcher = mapboxNavigation.getEventDispatcher();
+        String accessToken = mapboxNavigation.obtainAccessToken();
+        initializeRouteFetcher(dispatcher, accessToken, mapboxNavigation.retrieveEngineProvider());
+        initializeNotificationProvider(mapboxNavigation);
+        initializeRouteProcessorThread(dispatcher, routeFetcher, notificationProvider);
+        initializeLocationProvider(mapboxNavigation);
+    }
+
+    private void initializeRouteFetcher(NavigationEventDispatcher dispatcher, String accessToken,
+                                        NavigationEngineFactory engineProvider) {
+        FasterRoute fasterRouteEngine = engineProvider.retrieveFasterRouteEngine();
+        NavigationFasterRouteListener listener = new NavigationFasterRouteListener(dispatcher, fasterRouteEngine);
+        routeFetcher = new RouteFetcher(getApplication(), accessToken);
+        routeFetcher.addRouteListener(listener);
+    }
+
+    private void initializeNotificationProvider(MapboxNavigation mapboxNavigation) {
+        notificationProvider = new NavigationNotificationProvider(getApplication(), mapboxNavigation);
+    }
+
+    private void initializeRouteProcessorThread(NavigationEventDispatcher dispatcher, RouteFetcher routeFetcher,
+                                                NavigationNotificationProvider notificationProvider) {
+        RouteProcessorThreadListener listener = new RouteProcessorThreadListener(
+                dispatcher, routeFetcher, notificationProvider
+        );
+        thread = new RouteProcessorBackgroundThread(new Handler(), listener);
+    }
+
+    private void initializeLocationProvider(MapboxNavigation mapboxNavigation) {
+        LocationEngine locationEngine = mapboxNavigation.getLocationEngine();
+        int accuracyThreshold = mapboxNavigation.options().locationAcceptableAccuracyInMetersThreshold();
+        LocationValidator validator = new LocationValidator(accuracyThreshold);
+        NavigationLocationEngineListener listener = new NavigationLocationEngineListener(
+                thread, mapboxNavigation, locationEngine, validator
+        );
+        locationEngineUpdater = new NavigationLocationEngineUpdater(locationEngine, listener);
+    }
+
+    private void startForegroundNotification(NavigationNotification navigationNotification) {
+        final Notification notification = navigationNotification.getNotification();
+        final int notificationId = navigationNotification.getNotificationId();
+        notification.flags = Notification.FLAG_FOREGROUND_SERVICE;
+        startForeground(notificationId, notification);
+    }
+
+    class LocalBinder extends Binder {
+        NavigationService getService() {
+            Timber.d("Local binder called.");
+            return NavigationService.this;
+        }
+    }
 }
