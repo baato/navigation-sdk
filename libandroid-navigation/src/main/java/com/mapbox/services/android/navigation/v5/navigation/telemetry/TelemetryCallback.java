546b682a0207ad05f962bc9556155eff1da0b115
Ichchha Moktan <ichchhamoktan07@gmail.com>
2022-03-24 11:10:37 +0545

    delete unnecessary classes

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/telemetry/TelemetryCallback.java
+++
@@ -1,9 +0,0 @@
-package com.mapbox.services.android.navigation.v5.navigation.telemetry;
-
-
-interface TelemetryCallback {
-
-  void onBackground();
-
-  void onForeground();
-}
--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/telemetry/VisionBuildEvent.java
+++
@@ -1,5 +0,0 @@
-package com.mapbox.services.android.navigation.v5.navigation.telemetry;
-
-public interface VisionBuildEvent {
-  Event build();
-}
